70f6e0cbd2ad8110fd5512dcd39b711bf2da2964
madax <maximejoullin@gmail.com>
2020-06-24 21:54:59 +0200

    Addpassword security

--- model/user.php
+++ model/user.php
@@ -30,7 +30,7 @@ class User {
   }
 
   public function setPassword( $password, $password_confirm = false ) {
-    $this->password = $password;
+    $this->password = password_hash($password, PASSWORD_BCRYPT);
   }
 
   /***************************
