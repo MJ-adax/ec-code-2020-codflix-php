f62a7baa0d732da15f05fe35a4697e6826db2091
madax <maximejoullin@gmail.com>
2020-06-24 14:53:18 +0200

    Add running registration form, need improve but OK

--- controller/signupController.php
+++ controller/signupController.php
@@ -11,6 +11,27 @@ function signupPage() {
   $user     = new stdClass();
   $user->id = isset( $_SESSION['user_id'] ) ? $_SESSION['user_id'] : false;
 
+  $email = $_POST['email'];
+  $password =  $_POST['password'];
+  $password_confirm = $_POST['password_confirm'];
+
+  if ($password == $password_confirm)
+  {
+    if (!empty($_POST['email']) && !empty($_POST['password'])) {
+      $newUser = new User();
+      $newUser->setEmail( $email );
+      $newUser->setPassword( $password);
+      $newUser->createUser();
+    }
+  }
+  else 
+  {
+    echo 'MOT DE PASSE INCORRECT';
+  }
+
+  
+  
+
   if( !$user->id ):
     require('view/auth/signupView.php');
   else:
